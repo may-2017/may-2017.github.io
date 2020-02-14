c5a851ddf76eadf114b6b13255129ad8038e1a92
songmeizhu <smzppp@gmail.com>
2020-02-13 18:18:04 -0800

    add praxi 1
    Added praxi data to hp, updated the project page of summary and three columns

---
+++ scripts/revealer4.js
@@ -0,0 +1,22 @@
+(function() {
+// Fake loading.
+  setTimeout(init, 300);
+
+  function init() {
+    document.body.classList.remove('loading');
+
+    //************************ reveal on hero ********************************
+
+    var rev1 = new RevealFx(document.querySelector('#rev-1'), {
+      revealSettings : {
+        bgcolor: '#4744D2',
+        onCover: function(contentEl, revealerEl) {
+          contentEl.style.opacity = 1;
+        }
+      }
+    });
+    document.querySelector('#rev-1').style.opacity = 1;
+    rev1.reveal();
+
+  }
+})();
